bfd9890707f2d36707880bcd5cf1cc645f2a0f94
Mahesh <b.maheshbandaru5@gmail.com>
2019-02-18 16:37:57 +0530

    1st commit

---
+++ App1/src/main/java/com/nt/App1/App.java
@@ -0,0 +1,13 @@
+package com.nt.App1;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public static void name() {
+		
+	}
+}
